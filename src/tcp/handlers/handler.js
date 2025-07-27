a4120b4958fc7118e5325bd8af8e68325538cd22
Infelo <infelo453@gmail.com>
2025-07-27 16:28:19 +0545

    Change in handler

--- src/tcp/handlers/handler.js
+++ src/tcp/handlers/handler.js
@@ -15,12 +15,12 @@ class DataHandler {
         this.data = data.toString('hex');
         console.log(this.data);
         const usedProtcol = this.identifyer(this.data)
-        console.log(usedProtcol)
     }
 
     identifyer(data) {
         for (const protocol of this.protocols) {
-            if (protocol.start.includes(data.slice(0, 2).toString('hex'))) {
+            if (protocol.start.includes(data.slice(0, 3).toString('hex'))) {
+                console.log(protocol.name)
                 return protocol.name;
             }
         }
