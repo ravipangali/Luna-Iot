f1eb5e0feb75d77ca2b9e7eaa91636180b30bd4f
Infelo <infelo453@gmail.com>
2025-07-28 11:40:49 +0545

    GTO6 Data handler

--- src/tcp/handlers/gt06_handler.js
+++ src/tcp/handlers/gt06_handler.js
@@ -44,7 +44,9 @@ class GT06Handler {
 
         if (data.event.string === 'status') {
             // var status = new StatusModel();
-            console.log(`IMEI: ${data.imei}  Battery: ${data.voltageLevel}  Signal: ${data.gsmSigStrength}  Ignition: ${data.terminalInfo.ignition}  Charging: ${data.terminalInfo.charging}  Relay: ${data.terminalInfo.relayState}`);
+            const battery = this.getBattery(data.voltageLevel);
+            const signal = this.getSignal(data.gsmSigStrength);
+            console.log(`IMEI: ${data.imei}  Battery: ${battery}  Signal: ${signal}  Ignition: ${data.terminalInfo.ignition}  Charging: ${data.terminalInfo.charging}  Relay: ${data.terminalInfo.relayState}`);
             // status = await status.createData({
             //     deviceId: device.id,
             //     imei: data.imei,
@@ -62,6 +64,46 @@ class GT06Handler {
         else {
             console.log('SORRY WE DIDNT HANDLE THAT');
             console.log(data);
+        }
+    }
+
+    getBattery(data) {
+        data = data.toLowerCase();
+        switch (data) {
+            case 'no power':
+                return 0;
+            case 'extremely low battery':
+                return 1;
+            case 'very low battery':
+                return 2;
+            case 'low battery':
+                return 3;
+            case 'medium':
+                return 4;
+            case 'high':
+                return 5;
+            case 'very high':
+                return 6;
+            default: 
+                return 0;
+        }
+    }
+
+    getSignal(data) {
+        data = data.toLowerCase();
+        switch (data) {
+            case 'no signal':
+                return 0;
+            case 'extremely weak signal':
+                return 1;
+            case 'very weak signal':
+                return 2;
+            case 'good signal':
+                return 3;
+            case 'strong signal':
+                return 4;
+            default: 
+                return 0;
         }
     }
 
