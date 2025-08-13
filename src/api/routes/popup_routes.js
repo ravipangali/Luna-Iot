eabf84cafe03028821f7eac4d8cfd9b6d90067c6
Infelo <infelo453@gmail.com>
2025-08-13 20:52:33 +0545

    Pop Up

--- src/api/routes/popup_routes.js
+++ src/api/routes/popup_routes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router();
 const PopupController = require('../controllers/popup_controller');
 const { corsMiddleware } = require('../middleware/cors_middleware');
-const { AuthMiddleware } = require('../middleware/auth_middleware');
 
 router.use(corsMiddleware);
 
@@ -10,10 +9,10 @@ router.use(corsMiddleware);
 router.get('/popup/active', PopupController.getActivePopups);
 
 // Protected routes - require auth and Super Admin role
-router.get('/popup', AuthMiddleware.verifyToken, PopupController.getAllPopups);
-router.get('/popup/:id', AuthMiddleware.verifyToken, PopupController.getPopupById);
-router.post('/popup/create', AuthMiddleware.verifyToken, PopupController.createPopup);
-router.put('/popup/update/:id', AuthMiddleware.verifyToken, PopupController.updatePopup);
-router.delete('/popup/delete/:id', AuthMiddleware.verifyToken, PopupController.deletePopup);
+router.get('/popup', PopupController.getAllPopups);
+router.get('/popup/:id', PopupController.getPopupById);
+router.post('/popup/create', PopupController.createPopup);
+router.put('/popup/update/:id', PopupController.updatePopup);
+router.delete('/popup/delete/:id', PopupController.deletePopup);
 
 module.exports = router;
