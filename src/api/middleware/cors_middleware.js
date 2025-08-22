f7ae5d44c54a3360bb6f687aa570382a06155d3f
Infelo <infelo453@gmail.com>
2025-08-22 13:27:06 +0545

    CORS

--- src/api/middleware/cors_middleware.js
+++ src/api/middleware/cors_middleware.js
@@ -1,8 +1,9 @@
 const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-phone, x-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
+
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
     } else {
@@ -10,4 +11,4 @@ const corsMiddleware = (req, res, next) => {
     }
 }
 
-module.exports = {corsMiddleware};
+module.exports = { corsMiddleware };
